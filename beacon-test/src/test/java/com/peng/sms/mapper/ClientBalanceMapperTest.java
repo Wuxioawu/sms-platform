9b8628b557b1e98d9305e957921953eb82f46a29
wu peng <1761230580@qq.com>
2025-09-22 21:07:25 +0100

    convert data of ClientBalanceMapper

--- beacon-test/src/test/java/com/peng/sms/mapper/ClientBalanceMapperTest.java
+++ beacon-test/src/test/java/com/peng/sms/mapper/ClientBalanceMapperTest.java
@@ -1,9 +1,16 @@
 package com.peng.sms.mapper;
 
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.peng.sms.client.CacheClient;
+import com.peng.sms.entity.ClientBalance;
 import org.junit.jupiter.api.Test;
 import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
+
+import java.util.Map;
 
 import static org.junit.jupiter.api.Assertions.*;
 
@@ -11,8 +18,21 @@ import static org.junit.jupiter.api.Assertions.*;
 @RunWith(SpringRunner.class)
 class ClientBalanceMapperTest {
 
-    @Test
-    void findByClientId() {
+    @Autowired
+    private ClientBalanceMapper clientBalanceMapper;
 
+    @Autowired
+    private CacheClient cacheClient;
+
+    @Test
+    void findByClientId() throws JsonProcessingException {
+        ClientBalance clientBalance = clientBalanceMapper.findByClientId(1L);
+
+        System.out.println(clientBalance);
+
+        ObjectMapper objectMapper = new ObjectMapper();
+        Map map = objectMapper.readValue(objectMapper.writeValueAsString(clientBalance), Map.class);
+        
+        cacheClient.sadd("client_balance:1", map);
     }
 }
