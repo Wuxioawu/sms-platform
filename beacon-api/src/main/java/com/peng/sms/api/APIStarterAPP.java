b973cc40cc58705eac32e7f33fb981993a909ef4
wu peng <1761230580@qq.com>
2025-09-22 11:43:57 +0100

    finish the feign getway

--- beacon-api/src/main/java/com/peng/sms/api/APIStarterAPP.java
+++ beacon-api/src/main/java/com/peng/sms/api/APIStarterAPP.java
@@ -4,9 +4,11 @@ package com.peng.sms.api;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.openfeign.EnableFeignClients;
 
 @SpringBootApplication
 @EnableDiscoveryClient
+@EnableFeignClients
 public class APIStarterAPP {
     public static void main(String[] args) {
         SpringApplication.run(APIStarterAPP.class, args);
---
+++ beacon-api/src/main/java/com/peng/sms/api/client/BeaconCacheClient.java
@@ -0,0 +1,8 @@
+package com.peng.sms.api.client;
+
+import org.springframework.cloud.openfeign.FeignClient;
+
+@FeignClient(value = "beacon-cache")
+public class BeaconCacheClient {
+
+}
