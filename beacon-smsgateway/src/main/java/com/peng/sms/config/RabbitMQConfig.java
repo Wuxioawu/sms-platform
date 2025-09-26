d6170f9ff69b3b2330f74c78b7cb75372ffab5ac
wu peng <1761230580@qq.com>
2025-09-26 17:45:20 +0100

    start the sms gateway module

---
+++ beacon-smsgateway/src/main/java/com/peng/sms/config/RabbitMQConfig.java
@@ -0,0 +1,8 @@
+package com.peng.sms.config;
+
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class RabbitMQConfig {
+
+}
---
+++ beacon-smsgateway/src/main/java/com/peng/sms/mq/SmsGatewayListener.java
@@ -0,0 +1,23 @@
+package com.peng.sms.mq;
+
+import com.peng.sms.model.StandardSubmit;
+import com.rabbitmq.client.Channel;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.amqp.core.Message;
+import org.springframework.amqp.rabbit.annotation.RabbitListener;
+import org.springframework.stereotype.Component;
+
+import java.io.IOException;
+
+@Component
+@Slf4j
+public class SmsGatewayListener {
+
+    @RabbitListener(queues = "${gateway.sendtopic}")
+    public void consume(StandardSubmit submit, Channel channel, Message message) throws IOException, InterruptedException {
+        log.info("[SMS Gateway Module] Received message submit = {}", submit);
+        // =====================Complete interaction with the carrier: send one request, receive two responses==========================
+        channel.basicAck(message.getMessageProperties().getDeliveryTag(), false);
+    }
+
+}
